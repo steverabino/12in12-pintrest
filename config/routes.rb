42e0fc69a7c37a58dc8fbe6b1a3888afe313d8d0
Steve Smith <stevesmith2609@gmail.com>
2015-04-09 16:42:07 +0100

    add and configure votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :pins
+  resources :pins do
+    member do
+      put "like", to: "pins#upvote"
+    end
+  end
 
   root "pins#index"
   # The priority is based upon order of creation: first created -> highest priority.

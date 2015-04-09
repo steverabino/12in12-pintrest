e1b9c8f6f6c866312d0617989de2d6bc91252ecd
Steve Smith <stevesmith2609@gmail.com>
2015-04-09 17:13:20 +0100

    finish up styling

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
+  before_action :authenticate_user!, except: [:show, :index]
   def index
     @pins = Pin.all.order("created_at DESC")
   end
